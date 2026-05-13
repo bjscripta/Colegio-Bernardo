b3f261ab98bf5475d93199db41fb2566a9a38573
benjaOSAMA <benj.candia@duocuc.cl>
2026-05-13 14:45:36 -0400

    Correcion POM - Eureka

--- plataformaescolar/infraestructuredomain/eurekaServer/src/main/java/com/infraestructuredomain/eurekaServer/EurekaServerApplication.java
+++ plataformaescolar/infraestructuredomain/eurekaServer/src/main/java/com/infraestructuredomain/eurekaServer/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.paymentchain.eurekaServer;
+package com.infraestructuredomain.eurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- plataformaescolar/infraestructuredomain/eurekaServer/src/test/java/com/infraestructuredomain/eurekaServer/EurekaServerApplicationTests.java
+++ plataformaescolar/infraestructuredomain/eurekaServer/src/test/java/com/infraestructuredomain/eurekaServer/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.paymentchain.eurekaServer;
+package com.infraestructuredomain.eurekaServer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
